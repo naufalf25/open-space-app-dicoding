bf5b151a212ef8eda6da97fa17666ef574a4908d
Muhammad Naufal Farras <opal.farras@gmail.com>
2025-05-22 09:22:42 +0700

    initial commit

---
+++ src/states/talks/reducer.js
@@ -0,0 +1,32 @@
+/**
+ * @TODO: Define the reducer for the talks state
+ */
+
+import { ActionType } from './action';
+
+function talksReducer(talks = [], action = {}) {
+  switch (action.type) {
+    case ActionType.RECEIVE_TALKS:
+      return action.payload.talks;
+    case ActionType.ADD_TALK:
+      return [...talks, action.payload.talk];
+    case ActionType.TOGGLE_LIKE_TALK:
+      console.log(talks);
+      console.log(action.payload);
+      return talks.map((talk) => {
+        if (talk.id === action.payload.talkId) {
+          return {
+            ...talk,
+            likes: talk.likes.includes(action.payload.userId)
+              ? talk.likes.filter((id) => id !== action.payload.userId)
+              : talk.likes.concat([action.payload.userId]),
+          };
+        }
+        return talk;
+      });
+    default:
+      return talks;
+  }
+}
+
+export default talksReducer;
